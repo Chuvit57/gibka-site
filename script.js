4a53e477928d6363f0fadb1cc8b3e101e344a8d8
VITALY CHU <vitaliy69@mail.ru>
2021-04-07 10:06:26 +0300

    Made a version for large ecrons

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    // button-top
+var $btnTop = $(".button-top")
+$(window).on("scroll", function(){
+    if ($(window).scrollTop() >= 20){
+    $btnTop.fadeIn();
+    }else{
+    $btnTop.fadeOut();
+    }
+});
+
+$btnTop.on("click", function(){
+    $("html,body").animate({scrollTop:0}, 900)
+});
+
+
